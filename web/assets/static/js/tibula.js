dd4af8b3fffb43b0981773e0d9689dd43af63f0d
Ubaldo Porcheddu <ubaldo@eja.it>
2024-01-13 11:16:24 +0000

    add suneditor support

--- web/assets/static/js/tibula.js
+++ web/assets/static/js/tibula.js
@@ -1,13 +1,13 @@
 // Copyright (C) 2007-2024 by Ubaldo Porcheddu <ubaldo@eja.it>
 
 function tableRowCheck(obj) {
-	input = obj.querySelector("input")
+	input = obj.querySelector('input')
 	input.checked = !input.checked
 }
 
 function tableRowsCheck(obj) {
-	document.querySelectorAll("td").forEach(function(element) {
-		elementInput = element.querySelector("input")
+	document.querySelectorAll('td').forEach(function(element) {
+		elementInput = element.querySelector('input')
 		if (elementInput) {
 			elementInput.checked = obj.checked
 		}
@@ -19,29 +19,29 @@ function tableInputCheck(obj) {
 }
 
 function tableEdit(obj) {
-  document.querySelectorAll("td").forEach(function(element) {
-    elementInput = element.querySelector("input")
+  document.querySelectorAll('td').forEach(function(element) {
+    elementInput = element.querySelector('input')
 		if (elementInput) {
     	elementInput.checked = false
 		}
   })
-	input = obj.querySelector("input")
+	input = obj.querySelector('input')
   input.checked = true
 	document.getElementsByName('ejaAction').forEach(function(element) {
-		if (element.value == "edit") {
+		if (element.value == 'edit') {
 			element.click()
 		}
 	})
 }
 
 function fieldUpload(name) {
- var el = window._protected_reference = document.createElement("INPUT");
- el.type = "file";
+ var el = window._protected_reference = document.createElement('INPUT');
+ el.type = 'file';
  el.addEventListener('change', function(ev) {
   var input=ev.target;
   var reader = new FileReader();
   reader.onload = function() {
-   document.forms[0].elements["ejaValues["+name+"]"].value=reader.result
+   document.forms[0].elements['ejaValues['+name+']'].value=reader.result
   };
   reader.readAsText(input.files[0]);
  });
@@ -50,9 +50,9 @@ function fieldUpload(name) {
 }
 
 function fieldDownload(o, name) {
- var fileName=prompt("Save As");
+ var fileName=prompt('Save As');
  if (fileName) {
-  o.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent( document.forms[0].elements["ejaValues["+name+"]"].value ));
+  o.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent( document.forms[0].elements['ejaValues['+name+']'].value ));
   o.setAttribute('download', fileName);
   return true;
  } else {
@@ -60,10 +60,39 @@ function fieldDownload(o, name) {
  }
 }
 
+
+function fieldEditor(name) {
+	var o = document.getElementsByName('ejaValues['+name+']')[0]
+	if (! editors.hasOwnProperty(name)) {
+		editors[name] = SUNEDITOR.create(o, {
+			buttonList: [
+        ['fullScreen','undo', 'redo'],
+        ['font', 'fontSize', 'formatBlock'],
+        ['paragraphStyle', 'blockquote'],
+        ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+        ['fontColor', 'hiliteColor', 'textStyle'],
+        ['removeFormat'],
+        ['outdent', 'indent'],
+        ['align', 'horizontalRule', 'list', 'lineHeight'],
+        ['table', 'link', 'image', 'video', 'audio'],
+        ['showBlocks', 'codeView', 'print'],
+			]
+		})
+	}
+}
+
+var editors = [];
+
 var toasts = document.querySelectorAll('.toast');
 toasts.forEach(function (toast) {
 	var toastInstance = new bootstrap.Toast(toast);
 	setTimeout(function () {
 		toastInstance.hide();
 	}, 5000);
+});
+
+document.getElementById('ejaForm').addEventListener('submit', function(event) {
+	for (var key in editors) {
+		editors[key].save()
+	}
 });
